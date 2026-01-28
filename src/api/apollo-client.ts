60a4c5530f1ef3223f90de44108465aebdd6e08c
Ксюша Кисель <kkseniya2004@yahoo.com>
2026-01-28 18:27:24 +0300

    add apollo

--- src/api/apollo-client.ts
+++ src/api/apollo-client.ts
@@ -1,43 +1,11 @@
-// import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
-
-// const httpLink = new HttpLink({
-//   uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:3001/api/graphql',
-// });
-
-// export const client = new ApolloClient({
-//   link: httpLink,
-//   cache: new InMemoryCache(),
-// });
-
-'use client';
-
-import {
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri:
-    process.env.NEXT_PUBLIC_GRAPHQL_URL ||
-    'http://localhost:3001/api/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = process.env.NEXT_PUBLIC_DEV_TOKEN;
-
-  return {
-    headers: {
-      ...headers,
-      authorization: token
-        ? `Bearer ${token}`
-        : '',
-    },
-  };
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:3001/api/graphql',
 });
 
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: httpLink,
   cache: new InMemoryCache(),
 });
+
