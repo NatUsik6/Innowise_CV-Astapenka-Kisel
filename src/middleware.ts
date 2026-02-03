6e109736b65d932b2c29e738e3f57152ca2aa047
NatUsik6 <tashausik6@gmail.com>
2026-02-03 20:28:37 +0300

    feat: create middleware

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('access_token')?.value;
+  const { pathname } = request.nextUrl;
+
+  const authPages = ['/login', '/signup'];
+  const isAuthPage = authPages.includes(pathname);
+
+  if (!token && !isAuthPage) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  if (token && isAuthPage) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
