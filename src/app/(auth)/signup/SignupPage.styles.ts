3e1112c192f7bef360fd60519639cacf947116bf
NatUsik6 <tashausik6@gmail.com>
2026-02-05 16:05:02 +0300

    refactor: align project structure with FSD and fix hydration issues

---
+++ src/app/(auth)/signup/SignupPage.styles.ts
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const AuthWrapper = styled(Box)({
+  minHeight: '100vh',
+  display: 'flex',
+  alignItems: 'flex-start',
+  justifyContent: 'center',
+  backgroundColor: 'rgba(53, 53, 53, 1)',
+  paddingTop: '0.5rem',
+});
